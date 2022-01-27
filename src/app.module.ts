00516ab4e171be8e58ea8bff037c22205521f87f
Rifky Sultan Karisma Andriansyah <batuhc105@gmail.com>
2022-01-28 03:27:13 +0700

    add customer resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
